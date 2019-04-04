d54d5d1c7964cc5805abc47ecb9424a562f4e292
yanjinhuagoogd <yanjinhuamail@gmail.com>
2019-04-04 10:46:58 +0800

    first commit

---
+++ ffmpegSolution/ffmpegSample/Helper/FfmpegHelper.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace ffmpegSample.Helper
+{
+    public class FfmpegHelper
+    {
+        #region 模拟控制台信号需要使用的api
+
+        [DllImport("kernel32.dll")]
+        static extern bool GenerateConsoleCtrlEvent(int dwCtrlEvent, int dwProcessGroupId);
+
+        [DllImport("kernel32.dll")]
+        static extern bool SetConsoleCtrlHandler(IntPtr handlerRoutine, bool add);
+
+        [DllImport("kernel32.dll")]
+        static extern bool AttachConsole(int dwProcessId);
+
+        [DllImport("kernel32.dll")]
+        static extern bool FreeConsole();
+
+        #endregion
+        // ffmpeg进程
+        static Process p;
+
+        // ffmpeg.exe实体文件路径
+        static string ffmpegPath = AppDomain.CurrentDomain.BaseDirectory + "ffmpeg\\ffmpeg.exe";
+
+        /// <summary>
+        /// 功能: 开始录制
+        /// </summary>
+        public static void Start()
+        {
+            if (p == null)
+            {
+                p = new Process();
+                ProcessStartInfo startInfo = new ProcessStartInfo(ffmpegPath);
+                startInfo.UseShellExecute = false;
+                startInfo.WindowStyle = ProcessWindowStyle.Normal;
+                startInfo.CreateNoWindow = true;  //不显示dos程序窗口
+                startInfo.RedirectStandardInput = true;
+                startInfo.RedirectStandardOutput = true;
+                startInfo.RedirectStandardError = true;//把外部程序错误输出写到StandardError流中
+                startInfo.Arguments = "-f gdigrab -framerate 30 -offset_x 0 -offset_y 0 -video_size 1600x900 -i desktop " + DateTime.Now.ToString("yyyyMMddHHmmss") + ".mpg ";
+                p.StartInfo = startInfo;
+                p.Start();
+                //p.BeginErrorReadLine();//开始异步读取
+                //p.WaitForExit();//阻塞等待进程结束
+                //p.Close();//关闭进程
+                //p.Dispose();//释放资源
+            }
+        }
+
+        /// <summary>
+        /// 功能: 停止录制
+        /// </summary>
+        public static void Stop()
+        {
+            AttachConsole(p.Id);
+            SetConsoleCtrlHandler(IntPtr.Zero, true);
+            GenerateConsoleCtrlEvent(0, 0);
+            FreeConsole();
+            p = null;
+        }
+    }
+}
---
+++ ffmpegSolution/ffmpegSample/ViewModel/MainWidowVM.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using GalaSoft.MvvmLight.Messaging;
+using System.Windows.Input;
+using ffmpegSample.Helper;
+using System.Windows.Threading;
+using System.Diagnostics;
+
+namespace ffmpegSample.ViewModel
+{
+    public class MainWidowVM : ViewModelBase
+    {
+        private DispatcherTimer tm = new DispatcherTimer();
+        //定义全局变量
+        public int currentCount = 0;
+        /// <summary>
+        /// 开始录制计时
+        /// </summary>
+        private string myTime = "开始录制";
+        /// <summary>
+        /// 开始录制计时
+        /// </summary>
+        public string  MyTime
+        {
+            get { return myTime; }
+            set
+            {
+                myTime = value;
+                RaisePropertyChanged(() => MyTime);
+            }
+        }
+
+        /// <summary>
+        /// 获取或设置
+        /// </summary>
+        private bool isStart = true;
+        /// <summary>
+        /// 获取或设置
+        /// </summary>
+        public bool IsStart
+        {
+            get { return isStart; }
+            set
+            {
+                isStart = value;
+                RaisePropertyChanged(() => IsStart);
+            }
+        }
+
+
+        /// <summary>
+        /// 开始录制
+        /// </summary>
+        private ICommand myStart;
+        /// <summary>
+        /// 开始录制
+        /// </summary>
+        public ICommand MyStart
+        {
+            get
+            {
+                return myStart ?? (myStart = new RelayCommand<object>(p =>
+                           {
+                               IsStart = false;
+                               Messenger.Default.Send<string>("MsgHide", "MsgHide");
+                               FfmpegHelper.Start();
+                               tm.Tick += tm_Tick;
+                               tm.Interval = TimeSpan.FromSeconds(0.05);
+                               tm.Start();
+                           }, a =>
+            {
+                               return true;
+                           }));
+            }
+        }
+        private void tm_Tick(object sender, EventArgs e)
+        {
+            currentCount++;
+            MyTime = "开始录制(" + currentCount + "s)";
+        }
+        /// <summary>
+        /// 获取或设置
+        /// </summary>
+        private ICommand myStop;
+        /// <summary>
+        /// 获取或设置
+        /// </summary>
+        public ICommand MyStop
+        {
+            get
+            {
+                return myStop ?? (myStop = new RelayCommand<object>(p =>
+                           {
+                               IsStart = true;
+                               FfmpegHelper.Stop();
+                               MyTime = "开始录制";
+                               tm.Stop();
+                               currentCount = 0;
+                               Process.Start(AppDomain.CurrentDomain.BaseDirectory);
+                           }, a =>
+            {
+                               return true;
+                           }));
+            }
+        }
+
+        public MainWidowVM()
+        {
+
+        }
+        
+    }
+}
